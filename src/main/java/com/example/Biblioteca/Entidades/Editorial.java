f972fd2b47da0d58f04b16b573355328bde8aa20
juanpablo2025 <juanpablo_1810dev@hotmail.com>
2023-05-23 14:30:20 -0500

    se agregan lineas al app properties

--- src/main/java/com/example/Biblioteca/Entidades/Editorial.java
+++ src/main/java/com/example/Biblioteca/Entidades/Editorial.java
@@ -1,2 +1,51 @@
-package com.example.Biblioteca.Entidades;public class Editorial {
+package com.example.Biblioteca.Entidades;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name ="Editorial")
+public class Editorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "Nombre", unique =true)
+
+    private String nombre;
+    @Column(name = "Descripcion")
+    private String description;
+
+    public Editorial(Integer id, String nombre, String description) {
+        this.id = id;
+        this.nombre = nombre;
+        this.description = description;
+    }
+
+    public Editorial() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
