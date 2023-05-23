ba082d7a6d5e54f27f72d412d67072d5e95fcc1a
juanpablo2025 <juanpablo_1810dev@hotmail.com>
2023-05-23 14:40:53 -0500

    mod editorial

--- src/main/java/com/example/Biblioteca/Repositorios/RepositorioEditorial.java
+++ src/main/java/com/example/Biblioteca/Repositorios/RepositorioEditorial.java
@@ -1,2 +1,10 @@
-package com.example.Biblioteca.Repositorios;public class RepositorioEditorial {
+package com.example.Biblioteca.Repositorios;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.Biblioteca.Entidades.Editorial;
+
+@Repository
+public interface RepositorioEditorial extends JpaRepository<Editorial, Integer>{
 }
