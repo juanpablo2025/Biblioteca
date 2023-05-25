38e670cb7ebda336f5b046d7b3576c2dd3ee7085
juanpablo2025 <juanpablo_1810dev@hotmail.com>
2023-05-25 14:47:54 -0500

    se crea repositorioAutor

--- src/main/java/com/example/Biblioteca/Entidades/Autor.java
+++ src/main/java/com/example/Biblioteca/Entidades/Autor.java
@@ -1,2 +1,91 @@
-package com.example.Biblioteca.Entidades;public class Autor {
-}
+package com.example.Biblioteca.Entidades;
+
+import jakarta.persistence.*;
+
+
+
+    @Entity
+    @Table(name ="Autor")
+    public class Autor {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Integer id;
+        @Column(name = "Nombre", unique = true)
+
+        private String nombre;
+
+        @Column(name = "Apellidoo")
+        private String apellido;
+        @Column(name = "Pseudonimo")
+        private String pseudonimo;
+
+        @Column(name = "Nacionalidad")
+        private String nacionalidad;
+        @Column(name = "Email")
+        private String email;
+
+        public Autor(Integer id, String nombre, String apellido, String pseudonimo, String nacionalidad, String email) {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.pseudonimo = pseudonimo;
+            this.nacionalidad = nacionalidad;
+            this.email = email;
+
+
+        }
+
+        public Autor() {
+        }
+
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public String getApellido() {
+            return apellido;
+        }
+
+        public void setApellido(String apellido) {
+            this.apellido = apellido;
+        }
+
+        public String getPseudonimo() {
+            return pseudonimo;
+        }
+
+        public void setPseudonimo(String pseudonimo) {
+            this.pseudonimo = pseudonimo;
+        }
+
+        public String getNacionalidad() {
+            return nacionalidad;
+        }
+
+        public void setNacionalidad(String nacionalidad) {
+            this.nacionalidad = nacionalidad;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+
--- src/main/java/com/example/Biblioteca/Repositorios/RepositorioAutor.java
+++ src/main/java/com/example/Biblioteca/Repositorios/RepositorioAutor.java
@@ -1,2 +1,9 @@
-package com.example.Biblioteca.Repositorios;public interface RepositorioAutor {
+package com.example.Biblioteca.Repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.Biblioteca.Entidades.Autor;
+
+@Repository
+public interface RepositorioAutor extends JpaRepository<Autor,Integer> {
 }
