152128239bb669a4efbe8cd82bf804cbcca18293
juanpablo2025 <juanpablo_1810dev@hotmail.com>
2023-05-29 22:14:02 -0500

    Se crean Variables de la identidad Ubicacion

--- src/main/java/com/example/Biblioteca/Entidades/Ubicacion.java
+++ src/main/java/com/example/Biblioteca/Entidades/Ubicacion.java
@@ -1,2 +1,24 @@
-package com.example.Biblioteca.Entidades;public class Ubicavion {
+package com.example.Biblioteca.Entidades;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Ubicacion")
+public class Ubicacion {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+    
+	@Column(name = "Piso",max=20)
+    /*@Size(20)  varchar ?*/
+	private String Piso;
+    @Lob
+    @Column(name ="Salon",max=20)
+    final String Salon;
+
+    @Column(name="Estante",max=20)
+    private String estante;
+
 }
