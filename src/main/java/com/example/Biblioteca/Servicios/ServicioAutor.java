06e985d0f72504fc20d8e1992688d96af52e43f6
juanpablo2025 <juanpablo_1810dev@hotmail.com>
2023-05-30 12:34:20 -0500

    Se agregan los metodos al ServicioAutor

--- src/main/java/com/example/Biblioteca/Servicios/ServicioAutor.java
+++ src/main/java/com/example/Biblioteca/Servicios/ServicioAutor.java
@@ -5,6 +5,9 @@ import com.example.Biblioteca.Repositorios.RepositorioAutor;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
+
+import static com.example.Biblioteca.Repositorios.RepositorioAutor.*;
 
 public class ServicioAutor implements ServicioBase<Autor>{
 
@@ -25,22 +28,64 @@ public class ServicioAutor implements ServicioBase<Autor>{
 
     @Override
     public Autor buscarPorId(Integer id) throws Exception {
-        return null;
+        try{
+            Optional<Autor> autorOpcional =repositorioAutor.findById(id);
+            if(autorOpcional.isPresent()){
+                return autorOpcional.get();
+            }else{
+                throw new Exception("Usuario no encontrado");
+            }
+        }catch(Exception error){
+            throw new Exception(error.getMessage());
+        }
     }
 
     @Override
     public Autor registrar(Autor datosARegistrar) throws Exception {
-        return null;
+        try{
+
+            Autor autorGuardado= repositorioAutor.save(datosARegistrar);
+            return autorGuardado;
+
+        }catch(Exception error){
+            throw new Exception(error.getMessage());
+        }
     }
 
     @Override
     public Autor actualizar(Integer id, Autor datosNuevos) throws Exception {
-        return null;
+       try{
+           Optional<Autor>autorOptional=repositorioAutor.findById(id);
+           if (autorOptional.isPresent()){
+               Autor autorExistente=autorOptional.get();
+               autorExistente.setNombre(datosNuevos.getNombre());
+               autorExistente.setApellido(datosNuevos.getApellido());
+               autorExistente.setPseudonimo(datosNuevos.getPseudonimo());
+               autorExistente.setEmail(datosNuevos.getEmail());
+               autorExistente.setNacionalidad(datosNuevos.getNacionalidad());
+               Autor autorActualizado=repositorioAutor.save(autorExistente);
+               return autorActualizado;
+           }else{
+               throw new Exception("usuario no encontrado");
+           }
+       }catch(Exception error){
+           throw new Exception(error.getMessage());
+        }
     }
 
     @Override
     public boolean eliminar(Integer id) throws Exception {
-        return false;
+        try{
+            Optional<Autor>autorOptional=repositorioAutor.findById(id);
+            if(autorOptional.isPresent()){
+                repositorioAutor.deleteById(id);
+                return true;
+            }else{
+                throw new Exception("Usuario no encontrado");
+            }
+        }catch(Exception error){
+            throw new Exception(error.getMessage());
+        }
     }
 
 
