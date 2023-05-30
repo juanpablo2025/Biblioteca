49315041f1ca01ac1e8ebd43ffbeb5ada094eda4
juanpablo2025 <juanpablo_1810dev@hotmail.com>
2023-05-29 21:37:48 -0500

    se crean constructores,setters and getters

--- src/main/java/com/example/Biblioteca/Entidades/Categoria.java
+++ src/main/java/com/example/Biblioteca/Entidades/Categoria.java
@@ -1,2 +1,54 @@
-package com.example.Biblioteca.Entidades;public class Categoria {
+package com.example.Biblioteca.Entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+
+@Table(name = "Categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "Nombre", unique = true)
+	private String nombre;
+
+	@Column(name = "Description", max = 100)
+	private String descripcion;
+
+	public Categoria(Integer id, String nombre, String descripcion) {
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 }
